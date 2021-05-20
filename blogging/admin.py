015c5b8aef37c6a7a5b4d135db1db0847edea0c5
Rose Nyameke <nerozay.01@gmail.com>
2021-05-20 07:18:31 -0700

    add categories to posts but not vice versa

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
+class CategoryInline(admin.StackedInline):
+    model = Category.posts.through
+
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoryInline,
+    ]
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts',)
+
+
 # Register your models here.
-admin.site.register(Post)
-admin.site.register(Category)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
