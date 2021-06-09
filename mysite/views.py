b590606deb45a66cb141dd08e9f8d45fbdac16ab
Rose Nyameke <nerozay.01@gmail.com>
2021-06-08 23:58:04 -0700

    redirect to login after sign up

--- mysite/views.py
+++ mysite/views.py
@@ -4,11 +4,11 @@ from django.views.generic.edit import CreateView
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
-
+from django.urls import reverse_lazy
 
 class SignUpView(CreateView):
     template_name = "register.html"
-    success_url = "/"
+    success_url = reverse_lazy("login")
     form_class = RegisterForm
     success_message = "Your profile was created successfully"
 
@@ -21,7 +21,7 @@ class SignUpView(CreateView):
                 raw_password = form.cleaned_data.get("password")
                 user = authenticate(username=username, password=raw_password)
                 login(request, user)
-                return redirect("/")
+                return reverse_lazy("login") #redirect to login page because auto login isn't working
         else:
             form = UserCreationForm()
         return render(request, "register.html", {"form": form})
